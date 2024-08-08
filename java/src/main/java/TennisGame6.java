0b48b35ebedafb76da4568fad80d0f3d902f2f64
BOmokehinde <BOmokehinde@sis.tv>
2024-08-08 14:54:04 +0100

    changed to player1 and removed hard-coded string

--- java/src/main/java/TennisGame6.java
+++ java/src/main/java/TennisGame6.java
@@ -11,7 +11,7 @@ public class TennisGame6 implements TennisGame {
 
     @Override
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
+        if (playerName.equals(player1Name)) // change to player1Name for readability and removed the hard coded "player1" string
             player1Score++;
         else
             player2Score++;
